1e1de6426e8cdea35598c08bdfad5fa01b5fdce2
xrusnack <xrusnack@mail.muni.cz>
2023-03-30 08:59:21 +0200

    fixed docstring

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -12,11 +12,12 @@ def index():
     url = flask.request.args.get("url")
     return fetch_website(version, url)
 
-        
+
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
 
 
 class Person:
+    """Class Person that does who-knows-what"""
     def __init__(self, name):
         self.name = name
 
@@ -29,16 +30,16 @@ def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
- 
-    try: 
-        http = urllib.PoolManager()
+
+    try:
+        urllib.PoolManager()
     except:
         print('Exception')
 
 
 def load_yaml(filename):
     stream = open(filename)
-    deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
+    deserialized_data = yaml.load(stream, Loader=yaml.Loader)  # deserializing data
     return deserialized_data
 
 
@@ -50,13 +51,14 @@ def authenticate(password):
 
 if __name__ == '__main__':
     print("Vulnerabilities:")
-    print("1. Format string vulnerability: use string={person.__init__.__globals__[CONFIG][API_KEY]}")
+    print("""1. Format string vulnerability: 
+    use string={person.__init__.__globals__[CONFIG][API_KEY]}""")
     print("2. Code injection vulnerability: use string=;print('Own code executed') #")
     print("3. Yaml deserialization vulnerability: use string=file.yaml")
     print("4. Use of assert statements vulnerability: run program with -O argument")
-    choice  = input("Select vulnerability: ")
-    if choice == "1": 
-        new_person = Person("Vickie")  
+    choice = input("Select vulnerability: ")
+    if choice == "1":
+        new_person = Person("Vickie")
         print_nametag(input("Please format your nametag: "), new_person)
     elif choice == "2":
         urlib_version = input("Choose version of urllib: ")
@@ -67,4 +69,3 @@ if __name__ == '__main__':
     elif choice == "4":
         password = input("Enter master password: ")
         authenticate(password)
-
